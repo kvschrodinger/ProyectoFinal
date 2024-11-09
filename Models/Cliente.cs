b48fe69f11f988392b5e6356a55b6446edfc7a65
Carlos Cuellar <isnp153224@ugb.edu.sv>
2024-11-09 14:28:01 -0600

    API scalfold created

---
+++ Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public string DUI { get; set; }
+        public byte[] Foto { get; set; }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoFinal.Models
+{
+    
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+            public DbSet<Cliente> Clientes { get; set; }
+
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.Entity<Cliente>().HasKey(cliente => cliente.IdCliente);
+
+            }
+        }
+    }
+
+    
